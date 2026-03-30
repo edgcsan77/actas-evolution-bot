9210758c9e7e971d97c65b0a831ebd3394f92873
edgcsan77 <edgcsan@gmail.com>
2026-03-29 23:58:49 -0500

    Update queue.py

--- app/queue.py
+++ app/queue.py
@@ -2,5 +2,16 @@ from redis import Redis
 from rq import Queue
 from app.config import settings
 
-redis_conn = Redis.from_url(settings.REDIS_URL)
-request_queue = Queue("actas", connection=redis_conn)
+
+redis_conn = Redis.from_url(
+    settings.REDIS_URL,
+    socket_connect_timeout=5,
+    socket_timeout=5,
+    retry_on_timeout=True,
+)
+
+request_queue = Queue(
+    "actas",
+    connection=redis_conn,
+    default_timeout=300
+)
